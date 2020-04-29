2502e988b4d95b3a273bbb76ed103d5ed338f782
Alexandr Sapozhnikov <cawa.sapog@yandex.ru>
2020-04-29 16:03:38 +0400

    Bank

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,35 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
-        ListProduct listProduct = new ListProduct();
-        listProduct.add("apple", "sad pridonya", 5);
-        listProduct.add("orange", "7j", 6);
-        listProduct.add("apple", "sad pridonya", 5);
-        listProduct.add("orange", "7j", 6);
-        listProduct.add("apple", "sad pridonya", 5);
-        listProduct.add("orange", "7j", 6);
-        listProduct.add("apple", "sad pridonya", 5);
-        listProduct.add("orange", "7j", 6);
 
-        System.out.println(listProduct.size());
+    static void print(Thread t) {
+        System.out.println(t.getId());
+        System.out.println(t.getName());
+        System.out.println(t.getPriority());
+        System.out.println(t.isAlive());
+        System.out.println(t.getState());
+        System.out.println("*******");
+    }
 
-        System.out.println(listProduct.get("apple", "sad pridonya"));
+    public static void main(String[] args) throws InterruptedException {
+        Bank bank = new Bank(100);
 
-        listProduct.delete("apple", "sad pridonya",
-                listProduct.get("apple", "sad pridonya"));
+        System.out.println(bank.getTotal());
+        System.out.println("*******");
 
-        System.out.println(listProduct.size());
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        for (int i = 0; i < 20; i++) {
+            executorService.submit(new AccountOperation(bank));
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(60, TimeUnit.SECONDS);
+
+        System.out.println("*******");
+        System.out.println(bank.getTotal());
 
 
     }
